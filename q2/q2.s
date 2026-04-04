0c855a73ceea05f928f126a747d6ac8dcc1d6835
shreshthmehrotra89-sourc <shreshthmehrotra89@gmail.com>
2026-04-04 19:44:58 +0530

    Update q2.s

--- q2/q2.s
+++ q2/q2.s
@@ -1,79 +1,77 @@
 .section .text
 .globl main
 main:
-    addi sp, sp, -64
-    sd ra, 56(sp)
-    sd s0, 48(sp)
-    sd s1, 40(sp)
-    sd s2, 32(sp)
-    sd s3, 24(sp)
-    sd s4, 16(sp)
-    sd s5, 8(sp)
+    addi sp, sp, -80
+    sd ra, 72(sp)
+    sd s0, 64(sp)
+    sd s1, 56(sp)
+    sd s2, 48(sp)
+    sd s3, 40(sp)
+    sd s4, 32(sp)
+    sd s5, 24(sp)
+    sd s6, 16(sp)
 
-    mv s0, a0
-    mv s1, a1
-    addi s3, s0, -1
-    blez s3, exit
+    mv s3, a0            
+    mv s6, a1            
+    addi s3, s3, -1      
+    blez s3, exit_program
 
-    slli t0, s3, 3
-    mv a0, t0
+    slli a0, s3, 3
     call malloc
-    mv s0, a0
+    mv s0, a0            
 
-    slli t0, s3, 3
-    mv a0, t0
+    slli a0, s3, 3
     call malloc
-    mv s2, a0
+    mv s1, a0            
 
-    slli t0, s3, 3
-    mv a0, t0
+    slli a0, s3, 3
     call malloc
-    mv s1, a0
+    mv s2, a0            
 
-    li s4, 0
+    li s4, 0             
 parse_loop:
-    beq s4, s3, start_algo
-    addi t0, s4, 1
-    slli t0, t0, 3
-    add t0, s1, t0
-    ld a0, 0(t0)
+    beq s4, s3, algo_start
+    addi t0, s4, 1       
+    slli t0, t0, 3       
+    add t0, s6, t0       
+    ld a0, 0(t0)         
     call atoi
     slli t1, s4, 3
-    add t1, s0, t1
-    sd a0, 0(t1)
+    add t1, s0, t1       
+    sd a0, 0(t1)         
     addi s4, s4, 1
     j parse_loop
 
-start_algo:
-    addi s4, s3, -1
-    li s5, -1
+algo_start:
+    addi s4, s3, -1      
+    li s5, -1            
 outer_loop:
-    bltz s4, print_results
+    bltz s4, print_start
 while_loop:
-    bltz s5, set_result
+    bltz s5, finalize_step
     slli t0, s5, 3
     add t0, s2, t0
-    ld t1, 0(t0)
-    slli t2, t1, 3
+    ld t1, 0(t0)         
+    slli t1, t1, 3
+    add t1, s0, t1
+    ld t1, 0(t1)         
+    slli t2, t4, 3 # Dummy
+    slli t2, s4, 3
     add t2, s0, t2
-    ld t2, 0(t2)
-    slli t3, s4, 3
-    add t3, s0, t3
-    ld t3, 0(t3)
-    bgt t2, t3, set_result
-    addi s5, s5, -1
+    ld t2, 0(t2)         
+    bgt t1, t2, finalize_step
+    addi s5, s5, -1      
     j while_loop
-set_result:
+finalize_step:
     slli t0, s4, 3
-    add t0, s1, t0
+    add t0, s1, t0       
     li t1, -1
+    bltz s5, store_res
+    slli t1, s5, 3
+    add t1, s2, t1
+    ld t1, 0(t1)         
+store_res:
     sd t1, 0(t0)
-    bltz s5, push_idx
-    slli t2, s5, 3
-    add t2, s2, t2
-    ld t2, 0(t2)
-    sd t2, 0(t0)
-push_idx:
     addi s5, s5, 1
     slli t0, s5, 3
     add t0, s2, t0
@@ -81,10 +79,10 @@ push_idx:
     addi s4, s4, -1
     j outer_loop
 
-print_results:
+print_start:
     li s4, 0
 print_loop:
-    beq s4, s3, exit
+    beq s4, s3, exit_program
     slli t0, s4, 3
     add t0, s1, t0
     ld a1, 0(t0)
@@ -93,20 +91,21 @@ print_loop:
     addi s4, s4, 1
     j print_loop
 
-exit:
-    la a0, fmt_newline
+exit_program:
+    la a0, fmt_nl
     call printf
-    ld ra, 56(sp)
-    ld s0, 48(sp)
-    ld s1, 40(sp)
-    ld s2, 32(sp)
-    ld s3, 24(sp)
-    ld s4, 16(sp)
-    ld s5, 8(sp)
-    addi sp, sp, 64
+    ld ra, 72(sp)
+    ld s0, 64(sp)
+    ld s1, 56(sp)
+    ld s2, 48(sp)
+    ld s3, 40(sp)
+    ld s4, 32(sp)
+    ld s5, 24(sp)
+    ld s6, 16(sp)
+    addi sp, sp, 80
     li a0, 0
     ret
 
 .section .rodata
 fmt_int: .string "%d "
-fmt_newline: .string "\n"
+fmt_nl: .string "\n"
