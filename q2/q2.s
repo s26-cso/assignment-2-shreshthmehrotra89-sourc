746de0a14030d6b9d70239a9a8a5bcaa19556f84
shreshth <shreshthmehrotra89@gmail.com>
2026-04-16 00:54:00 +0530

    comment2

--- q2/q2.s
+++ q2/q2.s
@@ -3,13 +3,13 @@
 main:
     addi sp, sp, -80
     sd ra, 72(sp)
-    sd s0, 64(sp)
-    sd s1, 56(sp)
-    sd s2, 48(sp)
-    sd s3, 40(sp)
-    sd s4, 32(sp)
-    sd s5, 24(sp)
-    sd s6, 16(sp)
+    sd s0, 64(sp)  #for arr
+    sd s1, 56(sp)   #for nge arr
+    sd s2, 48(sp)  #stack
+    sd s3, 40(sp)  #n
+    sd s4, 32(sp)  #i
+    sd s5, 24(sp)  #stack top ptr
+    sd s6, 16(sp)  #argv base pointer
 
     mv s3, a0            
     mv s6, a1            
@@ -31,61 +31,61 @@ main:
     li s4, 0             
 parse_loop:
     beq s4, s3, algo_start
-    addi t0, s4, 1       
+    addi t0, s4, 1       #i+1
     slli t0, t0, 3       
     add t0, s6, t0       
-    ld a0, 0(t0)         
-    call atoi
+    ld a0, 0(t0)          #load number(input)
+    call atoi    #convert to integer
     slli t1, s4, 3
     add t1, s0, t1       
-    sd a0, 0(t1)         
-    addi s4, s4, 1
+    sd a0, 0(t1)         #arr[i]=input
+    addi s4, s4, 1         #i++
     j parse_loop
 
 algo_start:
     addi s4, s3, -1      
-    li s5, -1            
+    li s5, -1            #stack top 
 outer_loop:
     bltz s4, print_start
 while_loop:
     bltz s5, finalize_step
     slli t0, s5, 3
-    add t0, s2, t0
-    ld t1, 0(t0)         
-    slli t1, t1, 3
-    add t1, s0, t1
-    ld t1, 0(t1)         
-    slli t2, t4, 3 
-    slli t2, s4, 3
-    add t2, s0, t2
-    ld t2, 0(t2)         
-    bgt t1, t2, finalize_step
-    addi s5, s5, -1      
-    j while_loop
+    add t0, s2, t0  #stack address
+    ld t1, 0(t0)         #st.top()
+    slli t1, t1, 3   
+    add t1, s0, t1       
+    ld t1, 0(t1)         #arr[st.top()]
+    #slli t2, t4, 3 
+    slli t2, s4, 3         
+    add t2, s0, t2        
+    ld t2, 0(t2)         #arr[i]
+    bgt t1, t2, finalize_step  #arr[i]<arr[st.top()]
+    addi s5, s5, -1      #st.pop()
+    j while_loop          #while(arr[i]>arr[st.top()]) st.pop()
 finalize_step:
     slli t0, s4, 3
-    add t0, s1, t0       
-    li t1, -1
+    add t0, s1, t0    #nge[i]    
+    li t1, -1         #if st.empty() nge[i]=-1
     bltz s5, store_res
-    slli t1, s5, 3
+    slli t1, s5, 3       
     add t1, s2, t1
-    ld t1, 0(t1)         
+    ld t1, 0(t1)         #st.top()  (res=nge index)
 store_res:
-    sd t1, 0(t0)
-    addi s5, s5, 1
+    sd t1, 0(t0)  #nge[i]=res
+    addi s5, s5, 1  #top++
     slli t0, s5, 3
-    add t0, s2, t0
-    sd s4, 0(t0)
-    addi s4, s4, -1
+    add t0, s2, t0 
+    sd s4, 0(t0)   #st.push(i)
+    addi s4, s4, -1  #i--
     j outer_loop
 
 print_start:
     li s4, 0
 print_loop:
-    beq s4, s3, exit_program
+    beq s4, s3, exit_program  #i==n exit
     slli t0, s4, 3
     add t0, s1, t0
-    ld a1, 0(t0)
+    ld a1, 0(t0)   #nge[i]
     la a0, fmt_int
     call printf
     addi s4, s4, 1
